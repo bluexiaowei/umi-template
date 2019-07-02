71211f74373e837d97ded8ad09beb776527dfc4b
bluexiaowei <bluexiaowei@aliyun.com>
2019-07-02 14:01:38 +0800

    [FIX] 删除 webpack 配置 alias，alias 会导致 webpack 无法识别增量编译。 'src/*' 使用 '@/*' 自动识别

--- .umirc.js
+++ .umirc.js
@@ -1,7 +1,5 @@
 // ref: https://umijs.org/config/
 // ref: https://umijs.org/plugin/umi-plugin-react.html
-const path = require('path');
-const dirname = __dirname;
 const { URL, TITLE } = process.env;
 const umiPluginReact = {
   antd: true,
@@ -17,13 +15,6 @@ export default {
   history: 'hash',
   treeShaking: true,
   context: { URL, TITLE },
-  alias: {
-    Data: path.resolve(dirname, 'src/data/'),
-    Utils: path.resolve(dirname, 'src/utils/'),
-    Assets: path.resolve(dirname, 'src/assets/'),
-    Components: path.resolve(dirname, 'src/componets/'),
-    CommonLess: path.resolve(dirname, 'src/commonLess.less'),
-  },
   define: { BASE_URL: URL },
   publicPath: './',
   plugins: [['umi-plugin-react', umiPluginReact]],
